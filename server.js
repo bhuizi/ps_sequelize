a82f17b5a7e7875e0b908de202610a679e8498ac
bryan huizi <bryanhuizi@gmail.com>
2018-12-27 11:01:48 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Sequelize = require('sequelize');
+
+const app = express();
+const port = 8001;
+
+const connection = new Sequelize('db', 'user', 'pass', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    storage: 'db.sqlite',
+    operatorAliases: false
+});
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Connection to database established successully.')
+    })
+    .error(err => {
+        console.error('Unable to connect to the database', err);
+    });
+    
+app.listen(port, () => {
+    console.log(`Running server on port ${port}`)
+});

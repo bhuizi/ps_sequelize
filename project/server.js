a23f52b1d33afb654bf71a8d896c0a1d05077c75
bryan huizi <bryanhuizi@gmail.com>
2018-12-31 12:03:17 -0800

    sets up database

---
+++ project/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('./server/models/db');
+const app = express();
+const port = 3000;
+
+// setup the Express middlware
+require('./server/middleware/middleware')(app);
+
+// setup the api
+require('./server/api')(app);
+
+db.sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.log('running server on port ' + port);
+  })
+})
